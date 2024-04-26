fef7c284421a8a151d522a5bc343d4048f42f8fb
Ogunboyejo Olatunde <oogunboyejo@teamapt.com>
2024-04-26 23:06:28 +0100

    fix: Added landing page enhancements

--- admin-app/src/App.js
+++ admin-app/src/App.js
@@ -2,28 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import './App.css';
-import Dashboard from './components/Dashboard';
-import NotFound from './components/layout/NotFound';
-import PrivateRoute from './components/auth/PrivateRoute';
 import Register from './components/auth/Register';
 import SignIn from './components/auth/SignIn';
 import { LOGIN_USER } from './redux/actions/types';
 import Notification from './utils/Notifications';
 import setAuthToken from './utils/setAuthToken';
 import store from './store'
-import Profile from './components/user-profile/Profile';
-import Leaderboard from './components/leaderboard/Leaderboard';
-import Challenge from './components/challenge/Challenge';
-import QuestionDetails from './components/question/QuestionDetails';
-import AlgorithmChallengeAttemptDetails from './components/question/AlgorithmChallengeAttemptDetails';
-import ChallengeDetails from './components/challenge/ChallengeDetails';
 import Navbar from './components/layout/Navbar';
-import ChallengeInvite from './components/challenge/ChallengeInvite';
-import ChallengeLobbySection from './components/challenge/ChallengeLobbySection';
-import ChallengeSolutionDetails from './components/challenge/ChallengeSolutionDetails';
-import HomePage from './components/layout/HomePage';
-import WebSocket from './components/web-socket/WebSocket';
-import Sidebar from './components/layout/Sidebar';
 import AuthenticatedApp from './AuthenticatedApp';
 
 
@@ -47,7 +32,6 @@ function App() {
      <Router>
         <Navbar />
         <Notification />
-        <WebSocket />
           <Switch>
             <Route path='/login' exact component={SignIn} />
             <Route path='/register' exact component={Register} />
--- client/src/components/challenge/ChallengeDataTable.js
+++ client/src/components/challenge/ChallengeDataTable.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Table } from 'antd';
 import { Link } from 'react-router-dom';
-import data from '../../utils/challenges';
 
 const columns = [
   {
