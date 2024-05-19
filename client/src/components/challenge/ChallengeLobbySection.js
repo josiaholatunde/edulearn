a9c20e1e6e42da64659709916b11795732d56748
OGUNBOYEJO OLATUNDE <josiaholatunde17@gmail.com>
2024-05-19 15:13:39 +0100

    Update ChallengeLobbySection.js

--- client/src/components/challenge/ChallengeLobbySection.js
+++ client/src/components/challenge/ChallengeLobbySection.js
@@ -61,7 +61,7 @@ const ChallengeLobby = ({ history, challengeParticipants, challengeDetail, loadi
             id: challengeId
         }
         dispatch(handleChallengeUpdate(request, (res) => {
-            setTimeout(() => redirectToChallengeDetails(), 1555)
+            setTimeout(() => redirectToChallengeDetails(), 1551)
         }))
         
     }
